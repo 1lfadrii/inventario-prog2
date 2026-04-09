f070308714b40f27fe94ffd62cd5c44d9312622f
adrii <adrianmelendez839@gmail.com>
2026-04-08 19:37:27 -0600

    mejoras de presentación y mensajes al usuario

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import presentacion.Menu;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Bienvenido al Sistema de Inventario (v1.2)");
         Menu menu = new Menu();
         menu.mostrar();
     }
